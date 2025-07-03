a7c54e3d302871572ad7345ff6e20b322f687daa
Snehasish-iitd <snehasishbala.iitd28@gmail.com>
2025-07-03 11:58:04 +0530

    DK

--- testing/src/main/java/com/load/Service/LoadTestConfigService.java
+++ testing/src/main/java/com/load/Service/LoadTestConfigService.java
@@ -18,12 +18,28 @@ public class LoadTestConfigService {
 
     public LoadTestConfig createConfig(LoadTestConfigRequest request) {
         LoadTestConfig config = new LoadTestConfig();
+
         config.setCrudType(request.getCrudType());
         config.setTargetUrl(request.getTargetUrl());
         config.setNumUsers(request.getNumUsers());
         config.setRampUpPeriod(request.getRampUpPeriod());
         config.setTestDuration(request.getTestDuration());
         config.setScheduledExecutionTime(request.getScheduledExecutionTime());
+        config.setFileName(request.getRequestBodyFilePath());
+
+        
+        String filePath = request.getRequestBodyFilePath(); // e.g., "uploads/myfile.json"
+        String baseName = filePath;
+        if (baseName.contains("/")) baseName = baseName.substring(baseName.lastIndexOf('/') + 1);
+        if (baseName.contains(".")) baseName = baseName.substring(0, baseName.lastIndexOf('.'));
+
+        // Count previous runs for this file
+        int count = loadTestConfigRepository.countByFileName(filePath);
+        String testName = baseName + (count + 1);
+
+        config.setTestName(testName);
+
+    
         
         return loadTestConfigRepository.save(config);
     }
