66d14fe013637aa38ffbca0104fbc3bdfe47e05f
areucrazy27 <arpitrajputhm.09@gmail.com>
2025-07-03 16:44:55 +0530

    history , scedule api made [bugs 2.0]

--- tehelka_testing_app/lib/project.dart
+++ tehelka_testing_app/lib/project.dart
@@ -30,16 +30,40 @@ class Project {
 class ScheduledTest {
   final String testName;
   final DateTime scheduledDateTime;
+  final int id ;
+  final String targetUrl;
+  final int numUsers;
+  final int rampUpPeriod;
+  final int testDuration;
+  final String crudType;
+  final bool scheduled ;
+  final String createdAt; 
 
   ScheduledTest({
     required this.testName,
     required this.scheduledDateTime,
+    required this.id,
+    required this.targetUrl,
+    required this.numUsers,
+    required this.rampUpPeriod,
+    required this.testDuration,
+    required this.crudType,
+    required this.createdAt,
+    this.scheduled = false,
   });
 
   factory ScheduledTest.fromJson(Map json) {
     return ScheduledTest(
-      testName: json['testName'],
+      testName: json['testName'] ?? '',
       scheduledDateTime: DateTime.parse(json['scheduledDateTime']),
+      id: json['id'] ?? "",
+      targetUrl: json['targetUrl'] ?? '',
+      numUsers: json['numUsers'] ?? 0,
+      rampUpPeriod: json['rampUpPeriod'] ?? 0,
+      testDuration: json['testDuration'] ?? 0,
+      crudType: json['crudType'] ?? '',
+      createdAt: json['createdAt'] ?? '',
+      scheduled: json['scheduled'] ?? false,
     );
   }
 } 
